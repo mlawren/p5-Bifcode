0e693884f4d229345041a4b75ac7310e9e4f3ffb
Mark Lawrence <nomad@null.net>
2017-09-10 00:17:13 +0200

    Start development on version 0.001_9

--- lib/Bifcode.pm
+++ lib/Bifcode.pm
@@ -12,7 +12,7 @@ use Exporter::Tidy all => [
 
 # ABSTRACT: Serialisation similar to Bencode + undef/UTF8
 
-our $VERSION = '0.001_8';
+our $VERSION = '0.001_9';
 our ( $DEBUG, $max_depth, $dict_key );
 
 {
@@ -306,7 +306,7 @@ Bifcode - simple serialization format
 
 =head1 VERSION
 
-0.001_8 (2017-09-10)
+0.001_9 (yyyy-mm-dd)
 
 
 =head1 SYNOPSIS
