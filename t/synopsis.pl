5c4c37f6d72f573d0cafaaee956d47ecd6c5b20f
Mark Lawrence <nomad@null.net>
2022-01-18 12:11:46 +0100

    Rework t/synopsis.pl as a kind of test

--- t/synopsis.pl
+++ t/synopsis.pl
@@ -1,20 +1,13 @@
 #!/usr/bin/env perl
-use strict;
-use warnings;
-use utf8;
+use Test2::V0;
 use FindBin qw($RealBin);
 use lib "$RealBin/../lib";
-use lib "$RealBin/lib";
-use Bifcode2 qw( encode_bifcode2 decode_bifcode2 force_bifcode2 );
 use boolean;
-use Data::Dumper 'Dumper';
+use Bifcode2 qw( encode_bifcode2 decode_bifcode2 force_bifcode2 );
 use Path::Tiny;
-use Test::Bifcode;
-use Text::Table::Tiny 0.04 qw/ generate_table /;
+use Term::Table;
 
-no warnings 'once';
-
-my $str = q{encode_bifcode2 {
+my $struct = q{{
     bools   => [ boolean::false, boolean::true, ],
     bytes   => \pack( 's<',       255 ),
     integer => 25,
@@ -24,56 +17,53 @@ my $str = q{encode_bifcode2 {
 }
 };
 
-binmode STDOUT, ':utf8';
-print 'my $bifcode = ' . $str;
-my $bifcode = eval $str;
+my $ref      = eval $struct;
+my $bifcode2 = encode_bifcode2 $ref;
 
-binmode STDOUT, ':bytes';
-print $bifcode, "\n\n";
-print( ( eval $str . ',1' ), "\n\n" );
-my $bifcode_file = Path::Tiny->tempfile;
-$bifcode_file->spew_raw($bifcode);
+note 'my $bifcode2 = encode_bifcode2 ' . $struct;
+note $bifcode2;
+
+like( $bifcode2, qr/^{/, 'bifcode2 hash' );
+
+my $bifcode2_file = Path::Tiny->tempfile;
+$bifcode2_file->spew_raw($bifcode2);
 
 my $format      = '15/1 " %2x"' . "\n" . '"    " "%_p"' . "\n" . '"\n"' . "\n";
 my $format_file = Path::Tiny->tempfile;
 $format_file->spew($format);
-system( 'hexdump', '-f', $format_file, $bifcode_file );
+open my $fh, '-|', 'hexdump', '-f', $format_file, $bifcode2_file;
+note $_ while <$fh>;
 
-use constant TYPE    => 0;
-use constant PERL    => 1;
-use constant DISPLAY => 2;
+my $table_src = Term::Table->new(
+    header => [ 'Type', 'Perl', 'Bifcode2' ],
+    rows   => [
+        [ 'UNDEF',   'undef',          encode_bifcode2(undef) ],
+        [ 'TRUE',    'boolean::true',  encode_bifcode2(true) ],
+        [ 'FALSE',   'boolean::false', encode_bifcode2(false) ],
+        [ 'INTEGER', -1,               encode_bifcode2(-1) ],
+        [ 'INTEGER', 0,                encode_bifcode2(0) ],
+        [ 'INTEGER', 1,                encode_bifcode2(1) ],
 
-$Data::Dumper::Indent = 0;
-use Encode 'encode';
-my @items = ();
+        #    [ 'REAL',    '0.0', encode_bifcode2('0.0') ],
+        [ 'REAL', 3.1415,       encode_bifcode2(3.1415) ],
+        [ 'REAL', 1.380649e-23, encode_bifcode2(1.380649e-23) ],
+        [
+            'BYTES', q[$TWO_BYTE_STR],
+            encode_bifcode2( \pack( 's<', 255 ) ) =~ s/([.:]).*,/${1}��,/r
+        ],
+        [
+            'UTF8',
+            q{'MIXΣD ƬΣXƬ'},
+            encode_bifcode2('MIXΣD ƬΣXƬ') =~ s/([.:])(.*),/${1}MIXΣD ƬΣXƬ,/r
+        ],
+        [ 'ARRAY', q{[ 'one', 'two' ]}, encode_bifcode2( [ 'one', 'two' ] ) ],
+        [ 'DICT', q[{ key => 'value'}], encode_bifcode2( { key => 'value' } ) ],
 
-my $rows = [
-    [ 'Type',    'Perl',           'Bifcode' ],
-    [ 'UNDEF',   'undef',          encode_bifcode2(undef) ],
-    [ 'TRUE',    'boolean::true',  encode_bifcode2(true) ],
-    [ 'FALSE',   'boolean::false', encode_bifcode2(false) ],
-    [ 'INTEGER', -1,               encode_bifcode2(-1) ],
-    [ 'INTEGER', 0,                encode_bifcode2(0) ],
-    [ 'INTEGER', 1,                encode_bifcode2(1) ],
-
-    #    [ 'REAL',    '0.0', encode_bifcode2('0.0') ],
-    [ 'REAL',  3.1415,                encode_bifcode2(3.1415) ],
-    [ 'BYTES', q[\pack( 's<', 255 )], encode_bifcode2( pack( 's<', 255 ) ) ],
-    [
-        'UTF8', encode( 'UTF-8', q{'MIXΣD ƬΣXƬ'} ),
-        encode_bifcode2('MIXΣD ƬΣXƬ')
     ],
-    [ 'ARRAY', q{[ 'one', 'two' ]},  encode_bifcode2( [ 'one', 'two' ] ) ],
-    [ 'DICT',  q[{ key => 'value'}], encode_bifcode2( { key => 'value' } ) ],
-];
-
-my $T     = encode( 'UTF-8', 'Ƭ' );
-my $table = generate_table(
-    rows       => $rows,
-    header_row => 1
 );
-$table =~ s/$T'/$T'    /;
-$table =~ s/$T,/$T,    /;
-$table =~ s/(b2.*, )/$1 /;
 
-print $table. "\n";
+my $table = join( "\n", $table_src->render, '' );
+
+note($table);
+
+done_testing();
